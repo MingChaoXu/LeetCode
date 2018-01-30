f374bea6619833a853088dc7c6bc80146244e015
MingChaoXu <xumingchao_2013@163.com>
2018-01-30 21:55:44 +0800

    第一题和第二题

---
+++ pr2_Add_Two_Numbers.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        n = root = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            carry, val = divmod(carry, 10)
+            n = n.next = ListNode(val)
+        return root.next
